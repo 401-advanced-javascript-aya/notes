1cf5ea80900de5c5ccaff42e44fd2b14cafe1fe0
Aya-Akrabawi <aya.akrabawi.93@hotmail.com>
2020-10-12 23:16:13 +0300

    make the code clean

--- nodes.js
+++ nodes.js
@@ -3,9 +3,9 @@
 
 // require js files
 const Input = require('./lib/input.js');
-const HTTP = require('./lib/notes.js');
+const notes = require('./lib/notes.js');
 
 // Instantiates an instance of an “Input” parser module
 // (Parses the command line input and returns the command and data)
 const options = new Input();
-HTTP.fetch(options)
+notes.fetch(options)
