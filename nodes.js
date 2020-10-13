3ad84a0c2952a4b4cba5dfda02299c3985077b62
Aya-Akrabawi <aya.akrabawi.93@hotmail.com>
2020-10-13 14:24:11 +0300

    anything

--- nodes.js
+++ nodes.js
@@ -3,9 +3,11 @@
 
 // require js files
 const Input = require('./lib/input.js');
-const HTTP = require('./lib/notes.js');
+const Notes = require('./lib/notes.js');
 
 // Instantiates an instance of an “Input” parser module
 // (Parses the command line input and returns the command and data)
-const options = new Input();
-HTTP.fetch(options)
+const input = new Input();
+const notes = new Notes(input.data);
+
+notes.valid() ? notes.execute() : notes.help();
