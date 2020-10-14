c3a4a1106337063a93f3a502b3ff07f006f3e6f9
Aya-Akrabawi <aya.akrabawi.93@hotmail.com>
2020-10-15 00:05:14 +0300

    fix the code

--- nodes.js
+++ nodes.js
@@ -2,19 +2,9 @@
 'use strict';
 
 // require js files
-require('dotenv').config();
-const mongoose = require('mongoose');
 const Input = require('./lib/input.js');
 const Notes = require('./lib/notes.js');
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://aya93:good401@cluster0.o87pl.mongodb.net/notesy?retryWrites=true&w=majority' || 'mongodb://localhost:27017/notesy';
-
-mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }).catch((err) => {
-    console.log('Error related to database! ', err.message);
-  });;
 
 // Instantiates an instance of an “Input” parser module
 // (Parses the command line input and returns the command and data)
